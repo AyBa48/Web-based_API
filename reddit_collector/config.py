2cd0ff025427b513970246c504adf03069bf2b4c
Ayouba <ayoubah@gmail.com>
2023-05-29 13:45:14 +0200

    update

---
+++ reddit_collector/config.py
@@ -0,0 +1,6 @@
+tokens={
+"client_id":"aF6ul6hIDJwgCNfXPZ5LuA" , ## personal use script id
+"secret": "7qc-Wsk0jPzDDlKY8Smd_ACyou9A1Q",  ## secret
+"username": "ayoubah01" , # REDDIT USERNAME
+"password": "Spiced2023" # REDDIT PASSWORD
+}
